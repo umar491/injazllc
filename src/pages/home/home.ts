8c47be0eacba3eb82dd38909325d34a707cbf1f0
ummer9676 <umariqbal755@yahoo.com>
2017-10-24 18:19:55 +0400

    'message'

---
+++ src/pages/home/home.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  splash = true;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+ ionViewDidLoad() {
+    setTimeout(() => this.splash = false, 4000);
+  }
+}
